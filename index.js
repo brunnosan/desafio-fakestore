f44a7ff8ebad49b4e3fc148c5f7527f41342dea6
brunnosan <brunnosan@yahoo.com.br>
2024-02-17 05:14:51 -0300

    Versão 2

--- index.js
+++ index.js
@@ -3,13 +3,54 @@ busca.onkeyup = () => {
   .then(res=>res.json())
   .then(json=> {
     cards.innerHTML = '';
+    orderBy(json);
     json.forEach(card => {
-      if (card.title.toLowerCase().search(busca.value.toLowerCase()) != -1) {
-        addCard(card);
-      }
+      addCard(card);
     });
   })
     
+}
+
+sort.onchange = () => {
+  fetch('https://fakestoreapi.com/products')
+  .then(res=>res.json())
+  .then(json=> {
+    cards.innerHTML = '';
+    orderBy(json);
+    json.forEach(card => {
+      addCard(card);
+    });  
+ })
+}
+
+function addCard(card) {
+  if (card.title.toLowerCase().search(busca.value.toLowerCase()) != -1) {
+    console.log(card.title + ' - ' + card.rating.rate);
+    var novocard = document.createElement('div');
+    novocard.className = "card";
+    var textoHTML = '';
+    textoHTML += '      <div class="card-image">';
+    textoHTML += `        <img id="imagesrc2" src="${card.image}" alt="">`;
+    textoHTML += '      </div>';
+    textoHTML += '      <div class="card-content">';
+    textoHTML += `        <h2 id="price1">US$ ${card.price}</h2>`;
+    textoHTML += `        <i data-star="${card.rating.rate}"></i>`;
+    textoHTML += `        <h3 id="title1">${card.title}</h3>`;
+    textoHTML += `        <p id="description1">${card.description}</p>`;
+    textoHTML += '      </div>';
+    novocard.innerHTML = textoHTML;
+    cards.appendChild(novocard);
+  }
+}
+
+function orderBy(cards) {
+  if (sort.options[sort.selectedIndex].value == 'price-asc') {
+    cards.sort((menor, maior) => parseFloat(menor.price) - parseFloat(maior.price));
+  } else if (sort.options[sort.selectedIndex].value == 'price-desc') {
+    cards.sort((menor, maior) => parseFloat(maior.price) - parseFloat(menor.price));
+  } else if (sort.options[sort.selectedIndex].value == 'most-popular') {
+    cards.sort((menor, maior) => parseFloat(maior.rating.rate) - parseFloat(menor.rating.rate));
+  }
 }
 
 fetch('https://fakestoreapi.com/products')
@@ -19,18 +60,3 @@ fetch('https://fakestoreapi.com/products')
     addCard(card);
   });
 })
-
-function addCard(card) {
-  var novocard = document.createElement('div');
-  novocard.className = "card";
-  var textoHTML = '';
-  textoHTML += '      <div class="card-image">';
-  textoHTML += `        <img id="imagesrc2" src="${card.image}" alt="">`;
-  textoHTML += '      <div class="card-content">';
-  textoHTML += `        <h2 id="price1">US$ ${card.price}</h2>`;
-  textoHTML += `        <h3 id="title1">${card.title}</h3>`;
-  textoHTML += `        <p id="description1">${card.description}</p>`;
-  textoHTML += '      </div>';
-  novocard.innerHTML = textoHTML;
-  cards.appendChild(novocard);
-}
